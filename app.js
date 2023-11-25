8c98320144fab027971f046fd03e45a5d911eb9a
satyam-kumar-verma <billionairedolike@gmail.com>
2023-11-25 17:52:10 +0530

    Added internet checking logic and made page responsive

--- app.js
+++ app.js
@@ -1,88 +1,171 @@
-let searchInput = document.getElementById("searchInput");
-let resultCountries = document.getElementById("resultCountries");
+let searchInputEL = document.getElementById("searchInput");
+
+let resultContainer = document.getElementById("resultContainer");
+
 let loading = document.getElementById("spinner");
 
-function createAndAndAppendDetails(eachCountry) {
-    let countryContainer = document.createElement("div");
-    countryContainer.classList.add("col-11", "country-card", "d-flex", "flex-row", "col-md-5", "mr-auto", "ml-auto");
-    resultCountries.appendChild(countryContainer);
+let enemyCountry = document.getElementById("enemyCountry");
 
-    let countryFlag = document.createElement("img");
-    countryFlag.src = eachCountry.flag;
-    countryFlag.classList.add("country-flag");
-    countryContainer.appendChild(countryFlag);
+let searchInputValue;
 
-    let nameAndPopulationContainer = document.createElement("div");
-    nameAndPopulationContainer.classList.add("d-flex", "flex-column", "ml-auto", "mr-auto");
-    countryContainer.appendChild(nameAndPopulationContainer);
+let firstTimeNetworkCheck = false;
 
-    let nameEl = document.createElement("h1");
-    nameEl.classList.add("country-name");
-    nameEl.textContent = eachCountry.name;
-    nameAndPopulationContainer.appendChild(nameEl);
+let firstTimeAllow =  true;
 
-    let populationEl = document.createElement("p");
-    populationEl.classList.add("country-population");
-    populationEl.textContent = eachCountry.population;
-    nameAndPopulationContainer.appendChild(populationEl);
+let networkCheck = false;
+
+let allow =  true;
 
 
-}
+function createAndAppendDetails(eachItem){
 
-function getAllCountry(event, jsonData) {
-    loading.classList.toggle("d-none");
-    let inputValue = event.target.value;
-    if (inputValue === "") {
-        getAllCountryfirstTime();
-        return true;
+    let detailContainer = document.createElement("div");
+    detailContainer.classList.add("col-12", "col-md-5", "detail-container", "p-3", "mb-3");
+    resultContainer.appendChild(detailContainer);
+
+    let imgEl = document.createElement("img");
+    imgEl.classList.add("img-style");
+    imgEl.src = eachItem.flag ;
+    detailContainer.appendChild(imgEl);
+
+    let subDetailContainer = document.createElement("div");
+    subDetailContainer.classList.add("detail-sub-container");
+    detailContainer.appendChild(subDetailContainer);
+
+    let countryNameEl = document.createElement("p");
+    countryNameEl.classList.add("country-name-style");
+    countryNameEl.textContent = eachItem.name ;
+    subDetailContainer.appendChild(countryNameEl);
+
+    let countryPopulationEl = document.createElement("p");
+    countryPopulationEl.classList.add("country-population-style");
+    countryPopulationEl.textContent = eachItem.population ;
+    subDetailContainer.appendChild(countryPopulationEl);
+
+};
+
+function sendAllData(jsonData){
+
+    if(searchInputValue === ""){
+        FirstTimeFetchData();
     }
-    for (let eachCountry of jsonData) {
-        if (eachCountry.name === inputValue) {
-            createAndAndAppendDetails(eachCountry);
+    else{
+        for(let eachItem of jsonData){
+            if(eachItem.name === searchInputValue){
+                if(searchInputValue === "Pakistan" || searchInputValue === "China"){
+                    enemyCountry.classList.add("enemy-country");
+                    enemyCountry.textContent = `${searchInputValue} ki ammi ka bhosda`;
+                }
+                createAndAppendDetails(eachItem);
+            }
         }
     }
-}
+};
 
+function fetchData(event){
 
+    networkCheck = false;
+    allow = true;
 
-function fetchData(event) {
-    loading.classList.toggle("d-none");
-    resultCountries.textContent = "";
+    enemyCountry.textContent = "";
+    enemyCountry.classList.remove("enemy-country");
+    
+    resultContainer.textContent = "";
+    loading.classList.remove("d-none");
 
-    let inputValue = searchInput.value;
+    searchInputValue = searchInputEL.value;
 
-    let mainUrl = "https://apis.ccbp.in/countries-data";
+    let url = "https://apis.ccbp.in/countries-data";
     let options = {
         method: "GET"
-    }
-    fetch(mainUrl, options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonData) {
-            getAllCountry(event, jsonData);
-        })
+    };
+
+    fetch(url, options)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+
+        networkCheck = true;
+
+        loading.classList.add("d-none");
+
+        console.log(jsonData);
+
+        if(allow === true){
+            sendAllData(jsonData);
+        }
+    });
+
+    setTimeout(function(){
+        if(networkCheck === false){
+
+            allow = false;
+
+            loading.classList.add("d-none");
+
+            enemyCountry.textContent = "Check your internet connection";
+            enemyCountry.classList.add("enemy-country");
+
+        }
+    },5000);
+
 }
 
-function getAllCountryfirstTime() {
-    loading.classList.toggle("d-none");
+function sendFirstTimeAllData(jsonData){
 
-    let mainUrl = "https://apis.ccbp.in/countries-data";
+    for(let eachItem of jsonData){
+        createAndAppendDetails(eachItem);
+    }
+};
+
+function FirstTimeFetchData(){
+
+    firstTimeNetworkCheck = false;
+    firstTimeAllow = true;
+
+    enemyCountry.textContent = "";
+    enemyCountry.classList.remove("enemy-country");
+
+    resultContainer.textContent = "";
+    loading.classList.remove("d-none");
+
+    let url = "https://apis.ccbp.in/countries-data";
     let options = {
         method: "GET"
-    }
-    fetch(mainUrl, options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonData) {
-            loading.classList.toggle("d-none");
-            for (let eachCountry of jsonData) {
-                createAndAndAppendDetails(eachCountry);
-            }
-        })
+    };
+
+    fetch(url, options)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonData){
+
+        firstTimeNetworkCheck = true;
+
+        loading.classList.add("d-none");
+
+        if(firstTimeAllow === true){
+            sendFirstTimeAllData(jsonData);
+        }
+
+    });
+
+    setTimeout(function(){
+        if(firstTimeNetworkCheck === false){
+
+            firstTimeAllow = false;
+
+            loading.classList.add("d-none");
+
+            enemyCountry.textContent = "Check your internet connection";
+            enemyCountry.classList.add("enemy-country");
+
+        }
+    },5000);
+
 }
 
-getAllCountryfirstTime();
+FirstTimeFetchData();
 
-searchInput.addEventListener("keyup", fetchData);
+searchInputEL.addEventListener("keyup",fetchData);
